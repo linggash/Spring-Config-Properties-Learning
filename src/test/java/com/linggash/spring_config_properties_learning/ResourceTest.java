cbdf2d6ef66b20790fefcc91c1a4c3ba567cf901
linggash <sempanalingga@gmail.com>
2024-08-30 09:40:13 +0700

    Learned about Resource

--- src/test/java/com/linggash/spring_config_properties_learning/ResourceTest.java
+++ src/test/java/com/linggash/spring_config_properties_learning/ResourceTest.java
@@ -1,2 +1,20 @@
-package com.linggash.spring_config_properties_learning;public class ResourceTest {
+package com.linggash.spring_config_properties_learning;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class ResourceTest {
+
+    @Test
+    void resourceTest() throws IOException {
+        var resource = new ClassPathResource("/text/sample.txt");
+
+        Assertions.assertNotNull(resource);
+        Assertions.assertTrue(resource.exists());
+        Assertions.assertNotNull(resource.getFile());
+
+    }
 }
