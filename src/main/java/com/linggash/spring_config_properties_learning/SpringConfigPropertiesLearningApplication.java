025ae5530557cedc1729eced355d454e5e5c98fe
linggash <sempanalingga@gmail.com>
2024-08-30 09:38:14 +0700

    Init Project

---
+++ src/main/java/com/linggash/spring_config_properties_learning/SpringConfigPropertiesLearningApplication.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_config_properties_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigPropertiesLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigPropertiesLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linggash/spring_config_properties_learning/ResourceTest.java
@@ -0,0 +1,2 @@
+package com.linggash.spring_config_properties_learning;public class ResourceTest {
+}
