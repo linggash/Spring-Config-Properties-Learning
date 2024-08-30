6c8e8cd2f2acd0f536a8fb162d2c9d0a58291a3e
linggash <sempanalingga@gmail.com>
2024-08-30 17:16:25 +0700

    Learned about Collection Confiugration Properties

--- src/test/java/com/linggash/spring_config_properties_learning/configurationproperties/ConfigurationPropertiesTest.java
+++ src/test/java/com/linggash/spring_config_properties_learning/configurationproperties/ConfigurationPropertiesTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+
 @SpringBootTest(classes = ConfigurationPropertiesTest.TestApplication.class)
 public class ConfigurationPropertiesTest {
 
@@ -27,6 +29,27 @@ public class ConfigurationPropertiesTest {
         Assertions.assertEquals("katakunci", properties.getDatabase().getPassword());
         Assertions.assertEquals("basisdata", properties.getDatabase().getDatabase());
         Assertions.assertEquals("jdbc:contoh", properties.getDatabase().getUrl());
+    }
+
+    @Test
+    void testCollection() {
+        Assertions.assertEquals(Arrays.asList("produk", "pelanggan", "kategori"), properties.getDatabase().getWhiteListTables());
+        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("produk"));
+        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("pelanggan"));
+        Assertions.assertEquals(100, properties.getDatabase().getMaxTablesSize().get("kategori"));
+    }
+
+    @Test
+    void testEmbeddedCollection() {
+        Assertions.assertEquals("default", properties.getDefaultRoles().get(0).getId());
+        Assertions.assertEquals("Default Role", properties.getDefaultRoles().get(0).getName());
+        Assertions.assertEquals("default", properties.getDefaultRoles().get(1).getId());
+        Assertions.assertEquals("Default Role", properties.getDefaultRoles().get(1).getName());
+
+        Assertions.assertEquals("satu", properties.getRoles().get("satu").getId());
+        Assertions.assertEquals("utas", properties.getRoles().get("satu").getName());
+        Assertions.assertEquals("dua", properties.getRoles().get("dua").getId());
+        Assertions.assertEquals("uad", properties.getRoles().get("dua").getName());
     }
 
     @SpringBootApplication
